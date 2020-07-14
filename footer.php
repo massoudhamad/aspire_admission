541c6093acd2e6045b3c3a6e31b443841170b84c
Massoud Hamad <massoudhamad@gmail.com>
2020-07-14 14:00:48 +0300

    Footer Changes

--- footer.php
+++ footer.php
@@ -1,7 +1,23 @@
+<?php
+$db=new DBHelper();
+$org = $db->getRows("organization");
+if (!empty($org)) {
+        foreach ($org as $og) {
+                $orgName = $og['organizationName'];
+                $orgPicture = $og['organizationPicture'];
+                $orgPhone = $og['organizationPhone'];
+                $studentSupport = $og['student_support'];
+        }
+}
+else 
+{
+        $orgName = "Soft Dev Academy";
+}
+?>
 <footer class="main-footer navbar-fixed-bottom">
         <!-- To the right -->
         <div class="pull-left hidden-xs">
-This product is licensed to Muslim University of Morogoro
+This product is licensed to <?php echo $orgName;?>
         </div>
         <!-- Default to the left -->
         <div class="pull-right hidden-xs">
