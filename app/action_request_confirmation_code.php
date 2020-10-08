8434be98862ebcd496edd3c78d20664e5c701b2f
Massoud Hamad <massoudhamad@gmail.com>
2020-10-08 12:18:13 +0300

    Updates

--- app/action_request_confirmation_code.php
+++ app/action_request_confirmation_code.php
@@ -4,6 +4,16 @@ $db=new DBHelper();
 
 $formfour=$_POST['formfour'];
 $mobile_number=$_POST['mobile_number'];
+$email=$_POST['email_address'];
+
+$api_token = $db->getAPI("TCU", "token");
+if (!empty($api_token)) {
+    foreach ($api_token as $api) {
+        $token = $api['token'];
+        $user = $api['userName'];
+        $urlform = $api['url'];
+    }
+}
 
 
 $xml='<?xml version="1.0" encoding="UTF-8"?>
@@ -14,12 +24,15 @@ $xml='<?xml version="1.0" encoding="UTF-8"?>
 </UsernameToken>
 <RequestParameters>
 <f4indexno>'.$formfour.'</f4indexno>
-<MobileNumber>'.$mobile_number.'</MobileNumber>
+<MobileNumber>'.$mobile_number. '</MobileNumber>
+<EmailAddress>'.$email.'</ EmailAddress>
 </RequestParameters>
 </Request>';
 
+
+$url=$urlform."/admission/requestConfirmationCode";
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,"http://api.tcu.go.tz/admission/requestConfirmationCode");
+curl_setopt($ch, CURLOPT_URL,$url);
 curl_setopt($ch, CURLOPT_POST,1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
