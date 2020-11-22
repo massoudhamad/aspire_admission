4e19d586c09bb0eee7c802426e6e3523196c9635
Massoud Hamad <massoudhamad@gmail.com>
2020-11-22 18:29:18 +0300

    Updates

--- printform.php
+++ printform.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-/* ini_set ('display_errors', 1);
-error_reporting (E_ALL | E_STRICT);  */
+ini_set ('display_errors', 1);
+error_reporting (E_ALL | E_STRICT);  
 if($_REQUEST['action']=="getPDF")
 {
     include 'DB.php';
