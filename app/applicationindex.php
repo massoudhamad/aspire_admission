46e88ef07067008d4a1ecd78244cab00119adcdb
Massoud Hamad <massoudhamad@gmail.com>
2021-09-15 21:14:58 +0300

    Updates

--- app/applicationindex.php
+++ app/applicationindex.php
@@ -274,7 +274,7 @@ if ($login == 0) {
                                             }
                                             else if($_SESSION['orgCode']=="ZU")
                                             {
-                                                if($$programmeCode=="ZU022")
+                                                if($programmeCode=="ZU022")
                                                 {
                                                     ?>
                                                     <td>
@@ -288,7 +288,7 @@ if ($login == 0) {
                                                 ?>
                                                 <td>
                                                     <a href="printadmissionletterzu.php?action=getPDF&applicantID=<?php echo $applicantID; ?>" target="_blank">
-                                                        <span class="text text-success">Download Admission Letter</span></a>
+                                                        <span class="text text-success">Download Admission Letter333</span></a>
                                                     <br><br>
                                                 </td>
                                                 <?php 
