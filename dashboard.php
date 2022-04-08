2859bdd7ce2783f53e5d8ddca21f7bd56b9b9522
massoudhamad <massoudhamad@gmail.com>
2022-04-08 16:34:28 +0300

    Updates

--- dashboard.php
+++ dashboard.php
@@ -686,10 +686,98 @@ if(!empty($activeInTake)) {
                   <!-- general form elements -->
               <div class="box">
             <div class="box-header">
-              <h3 class="box-title">Application Summary by Schools</h3>
+              <h3 class="box-title">Application Summary by Programmes</h3>
             </div>
             <!-- /.box-header -->
-            <div class="box-body no-padding">
+
+            <?php
+$db = new DBHelper();
+$applicationYear=$db->getRows('academicyears',array('where'=>array('academicYearStatus'=>1),'order_by'=>'academicYearID'));
+    foreach ($applicationYear as $appYear) {
+        $applicationYearID=$appYear['academicYearID'];
+    }
+
+$activeInTake=$db->getRows("admission_setting",array('where'=>array('academicYearID'=>$applicationYearID,'yearStatus'=>1),'order_by academicYearID'));
+if(!empty($activeInTake)) {
+    foreach ($activeInTake as $intake) {
+        $admissionID = $intake['admissionID'];
+        $admissionInTakeID = $intake['admissionInTakeID'];
+    }
+}
+?>
+<div class="box-body no-padding">
+              <table class="table table-condensed">
+                    <thead>
+                    <tr>
+                  <th style="width: 10px">#</th>
+                  <th>Programmes Name</th>
+                  <th>Complete</th>
+                  <th>Incomplete</th>
+                  <th>Approved</th>
+                  <th>Admitted</th>
+                  <th>Rejected</th>
+                  <th>Registered</th>
+                  
+                    </tr></thead>
+                <tbody>
+                <?php
+                 $schools=$db->getRows('programs',array('where'=>array('programStatus'=>1),'order_by'=>'studyLevelID ASC'));
+                 $count=0;$totalC=0;$totalAd=0;$totalApp=0;$totalRej=0;$totalReg=0;$totalInc=0;
+                 foreach ($schools as $sch)
+                 {
+                     $count++;
+                     $programmeID=$sch['programID'];
+                     $programmeName=$sch['programName'];
+                     
+                     $registered=$db->getProgrammeCount($programmeID,6,$applicationYearID,$admissionID);
+                     $rejected=$db->getProgrammeCount($programmeID,4,$applicationYearID,$admissionID);
+                     $admitted=$db->getProgrammeCount($programmeID,3,$applicationYearID,$admissionID);
+                     $approved=$db->getProgrammeCount($programmeID,2,$applicationYearID,$admissionID);
+                     $complete=$db->getProgrammeCount($programmeID,1,$applicationYearID,$admissionID);
+                     $incomplete=$db->getSchoolCount($programmeID,5,$applicationYearID,$admissionID);
+                     
+                     $tadmitted=$registered+$admitted; 
+                     $tapproved=$tadmitted+$approved+$rejected+$registered;
+                     $tincomplete=$incomplete;
+                     $trejected=$rejected;
+                     $tcomplete=$approved+$admitted+$rejected+$registered+$incomplete+$complete;
+                     $totalInc+=$tincomplete;
+                     $totalC+=$tcomplete;
+                     $totalApp+=$tapproved;
+                     $totalAd+=$tadmitted;
+                     $totalRej+=$trejeceted;
+                     $totalReg+=$registered;
+                     $totalRej+=$rejected;
+                     ?>
+                <tr>
+                  <td><?php echo $count;?></td>
+                  <td><a href="index3.php?sp=listbyprogrammes&programmeID=<?php echo $programmeID;?>&admissionID=<?php echo $admissionID;?>">
+                  <?php echo $programmeName;?></a></td>
+                  <td><?php echo $tcomplete;?></td>
+                  <td><?php echo $tincomplete;?></td>
+                  <td><?php echo $tapproved;?></td>
+                  <td><?php echo $tadmitted;?></td>
+                  <td><?php echo $trejected;?></td>
+                  <td><?php echo $registered;?></td>
+                </tr>
+                <?php 
+                 }
+                ?>
+                 </tbody>
+                 <tfoot>
+                 <th></th><th>Total</th>
+                 <th><?php echo $totalC;?></th>
+                 <th><?php echo $totalInc;?></th>
+                <th><?php echo $totalApp;?>    </th>
+                <th><?php echo $totalAd;?></th>
+                <th><?php echo $totalRej;?></th>
+                <th><?php echo $totalReg;?></th>
+                </tfoot>
+              </table>
+      </div>
+
+
+            <!-- <div class="box-body no-padding">
               <table class="table table-condensed">
                 <tr>
                   <th style="width: 10px">#</th>
@@ -701,9 +789,9 @@ if(!empty($activeInTake)) {
                   <th>Rejected</th>
                   <th>Registered</th>
                   
-                </tr>
+                </tr> -->
                 <?php
-                 $schools=$db->getRows('schools',array('order_by'=>'schoolName ASC'));
+                 /* $schools=$db->getRows('schools',array('order_by'=>'schoolName ASC'));
                  $count=0;$totalC=0;$totalAd=0;$totalApp=0;$totalRej=0;$totalReg=0;$totalInc=0;
                  if(!empty($schools))
                  {
@@ -731,9 +819,9 @@ if(!empty($activeInTake)) {
                      $totalAd+=$tadmitted;
                      $totalRej+=$trejected;
                      $totalInc+=$tincomplete;
-                     $totalReg+=$registered;
+                     $totalReg+=$registered; */
                      ?>
-                <tr>
+                <!-- <tr>
                   <td><?php echo $count;?></td>
                   <td><a href='index3.php?sp=listbyschool&id=<?php echo $schoolID;?>'><?php echo $schoolName;?></a></td>
                   <td><?php echo $tcomplete;?></td>
@@ -742,12 +830,12 @@ if(!empty($activeInTake)) {
                   <td><?php echo $tadmitted;?></td>
                   <td><?php echo $trejected;?></td>
                   <td><?php echo $registered;?></td>
-                </tr>
+                </tr> -->
                 <?php 
-                 }
-               }
+                 /* }
+               } */
                 ?>
-                <th></th><th>Total</th>
+               <!--  <th></th><th>Total</th>
                 <th><?php echo $totalC;?></th>
                 <th><?php echo $totalInc;?></th>
                 <th><?php echo $totalApp;?></th>
@@ -755,9 +843,9 @@ if(!empty($activeInTake)) {
                 <th><?php echo $totalRej;?></th>
                 <th><?php echo $totalReg;?></th>
               </table>
-            </div>
+            </div> -->
             <!-- /.box-body -->
-            <br><br>
+            <!-- <br><br>
             <div class="box-footer">
                 <div class="col-lg-9"> </div>
                
@@ -765,7 +853,7 @@ if(!empty($activeInTake)) {
                
               </div>
            
-          </div>
+          </div> -->
           <!-- /.box -->
         </div>
                
