146544749dcdcc58297e9db6b541f14bc34cc4c3
massoudhamad <massoudhamad@gmail.com>
2022-09-02 17:52:44 +0300

    Updates

--- data/nacte_upload_report.php
+++ data/nacte_upload_report.php
@@ -27,7 +27,7 @@ if(!empty($applicantsData))
 
         if(empty($email))
         {
-            $lname."_".$fname."@gmail.com";
+            $email=$lname."_".$fname."@gmail.com";
         }
 
         $disabilityStatus=$row['disabilityStatus'];
