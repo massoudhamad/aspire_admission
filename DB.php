4c65af872aa241a6a9e48d9b4332927bc403be9a
massoudhamad <massoudhamad@gmail.com>
2022-12-11 18:12:48 +0300

    Updates

--- DB.php
+++ DB.php
@@ -2682,7 +2682,7 @@ WHERE
             if($roundname=='all')
             {
                 $query = $this->conn->prepare("SELECT 
-            DISTINCT(a.applicantID), firstName, middleName, lastName, gender,phoneNumber,dateOfBirth as dob,email,citizenship,disabilityStatus,entryQualification
+            DISTINCT(a.applicantID), firstName, middleName, lastName, gender,phoneNumber,dateOfBirth as dob,email,citizenship,disabilityStatus,entryQualification,formfour
         from
             applicants a,
             programmemajor pm,
@@ -2701,7 +2701,7 @@ WHERE
             else 
             {
                 $query = $this->conn->prepare("SELECT 
-            DISTINCT(a.applicantID), firstName, middleName, lastName, gender,phoneNumber,dateOfBirth as dob,email,citizenship,disabilityStatus,entryQualification
+            DISTINCT(a.applicantID), firstName, middleName, lastName, gender,phoneNumber,dateOfBirth as dob,email,citizenship,disabilityStatus,entryQualification,formfour
         from
             applicants a,
             programmemajor pm,
