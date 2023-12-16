53e2185ff2c46b7e2aaafa259054f7b5fe677ca0
massoudhamad <massoudhamad@gmail.com>
2023-12-16 17:47:49 +0300

    updates admit

--- action_admit.php
+++ action_admit.php
@@ -35,6 +35,61 @@ $tblRemarks='applicantremarks';
                      $insert = $db->insert($tblRemarks, $appData);
                      $status = true;
                      $jj++;
+
+                     //send mail
+        $org = $db->getRows("organization");
+        if (!empty($org)) {
+            foreach ($org as $og) {
+                $orgName = $og['organizationName'];
+                $studentSupport = $og['student_support'];
+                $orgemail=$og['organizationEmail'];
+            }
+        }
+        $applicantdata=$db->getRows("applicants",array('where'=>array('applicantID'=>$applicantID)));
+        if(!empty($applicantdata))
+        {
+            foreach($applicantdata as $dt)
+            {
+                $userID=$dt['userID'];
+                $fname=$dt['firstName'];
+                $lname=$dt['lastName'];
+
+            }
+        }
+        $name=$fname." ".$lname;
+        $email=$db->getData('users','email','userID',$userID);
+        $to = $email;
+        $subject = 'Login details for Aspire UAS Online Admission';
+        $from = 'info@hmytechnologies.com';
+
+        // To send HTML mail, the Content-type header must be set
+        $headers = 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+        // Create email headers
+        $headers .= 'From: ' . $from . "\r\n" .
+            'Reply-To: ' . $from . "\r\n" .
+            'CC: '.$orgemail.
+            'X-Mailer: PHP/' . phpversion();
+        // Compose a simple HTML email message
+        $message = '<html><body>';
+        $message .= '<h1 style="color:#080;">Email From: '.$orgName.'</h1>';
+        $message .= '<h2 style="color:#080;">Dear ' . $name . 'an applicant from '.$orgName. '</h1>';
+        $message .= '<p>There is changes in your application portal, please log in using your Username/IndexNumber(S000/0000/YYYY/YYYY) and Password(LASTNAME) to view the status of your application<br>For more information, please contact'. $studentSupport.'</p>';
+        $message .= '<p>We hope you enjoy using Aspire UAS and all services offered by other software solutions under SkyChuo package.</p>';
+        $message .= '<p></p>';
+        $message .= '<p>Warm Regards,</p>';
+        $message .= '<p></p>';
+        $message .= '<p>_________________________</p>';
+        $message .= '<p>'.$orgName.'</p>';
+        $message .= '<p>SkyChuo Account Management Services </p>';
+        $message .= '<p>SkyChuo is offered by <a href="http://www.hmytechnologies.com" target="_blank">HM&Y Technologies</a></p>';
+        $message .= '</body></html>';
+        // Sending email
+        mail($to, $subject, $message, $headers);
+
+        $boolStatus = true;
+
          }
             if($status)
             {
